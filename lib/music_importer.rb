a10a4515d4bbc2888a7acc5a7a984183d84d0821
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-08 00:44:34 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,41 +1,21 @@
-# class MusicImporter
-# 
-#   def initialize(path)
-#     @path = path
-#   end
-# 
-#   def path
-#     @path
-#   end
-# 
-#   def files()
-#     @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
-#   end
-# 
-#   def import()
-#     files.each do |filename|
-#       Song.create_from_filename(filename)
-#     end
-#   end
-# 
-# end# 
-
-require 'pry'
-
-class MusicImporter
-  attr_accessor :path, :files
-
-  def initialize(path)
-    @path = path
-  end
-
-  def files
-      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-  end
-
-  def import
-    files.each {|file| Song.create_from_filename(file) }
-  end
-
-
-end
+ class MusicImporter
+ 
+   def initialize(path)
+     @path = path
+   end
+ 
+   def path
+     @path
+   end
+ 
+   def files()
+     @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "")}
+   end
+ 
+   def import()
+     files.each do |filename|
+       Song.create_from_filename(filename)
+     end
+   end
+ 
+ end
