01f5e9eb2e760278f7d11fbbb4e9f3ee5270ee71
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-08 03:07:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@
    
    @@all = []
    
-   def initialize(name, artist = nil)
+   def initialize(name, artist = nil, genre = nil)
      @name = name 
      self.artist = artist if artist
      self.genre = genre if genre
