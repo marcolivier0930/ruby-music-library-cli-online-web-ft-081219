efb12a3265ca2004749efb8013df99c61dabae5f
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-08 00:45:50 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,7 @@
-# require 'bundler'
-# Bundler.require
-# 
-# module Concerns
-# end
-# 
-# require_all 'lib'
-
-require 'bundler'
-Bundler.require
-
- require_relative "../lib/song.rb"
- require_relative "../lib/artist.rb"
- require_relative "../lib/genre.rb"
-
-module Concerns
-end
+ require 'bundler'
+ Bundler.require
+ 
+ module Concerns
+ end
+ 
+ require_all 'lib'
