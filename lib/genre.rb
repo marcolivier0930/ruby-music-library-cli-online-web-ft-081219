0b8838b8619baaf5077e2f5ac21e61863d118b8a
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-08 00:44:59 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,89 +1,48 @@
-# class Genre 
-#   
-#   extend Concerns::Findable
-#   
-#   attr_accessor :name 
-#   @@all = []
-#   
-#   def initialize(name)
-#     @name = name 
-#     @songs = []
-#   end
-#   
-#   def self.all
-#     @@all
-#   end
-# 
-#   def self.destroy_all
-#     @@all.clear
-#   end
-#   def save
-#     @@all << self
-#   end
-# 
-#   def self.create(name)
-#     self.new(name).tap do |genre|
-#       genre.save
-#     end
-#   end
-# 
-#   def songs
-#     @songs
-#   end
-# 
-# 
-#   def add_song(song)
-#     song.genre = self unless song.genre == self
-#     @songs << song unless @songs.include?(song)
-#   end
-# 
-# 
-#   def artists
-#     artists = @songs.collect do |song|
-#       song.artist
-#     end
-#     artists.uniq
-#   end
-#   
-# end
-
-require 'pry'
-
-class Genre
-  extend Concerns::Findable
-  attr_accessor :name
-  attr_reader :songs
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    genre = self.new(name)
-    genre.save
-    genre
-  end
-
-  def songs
-    @songs
-  end
-
-  def artists
-    self.songs.collect {|s| s.artist}.uniq
-  end
-end
+ class Genre 
+   
+   extend Concerns::Findable
+   
+   attr_accessor :name 
+   @@all = []
+   
+   def initialize(name)
+     @name = name 
+     @songs = []
+   end
+   
+   def self.all
+     @@all
+   end
+ 
+   def self.destroy_all
+     @@all.clear
+   end
+   def save
+     @@all << self
+   end
+ 
+   def self.create(name)
+     self.new(name).tap do |genre|
+       genre.save
+     end
+   end
+ 
+   def songs
+     @songs
+   end
+ 
+ 
+   def add_song(song)
+     song.genre = self unless song.genre == self
+     @songs << song unless @songs.include?(song)
+   end
+ 
+ 
+   def artists
+     artists = @songs.collect do |song|
+       song.artist
+     end
+     artists.uniq
+   end
+   
+ end
